dac4cf13a9f18fb4ab5afcfa078b885a07a3ade9
gautam-193723 <gautammahajan193723@gmail.com>
2024-11-01 15:05:10 +0530

    user chage

--- App.js
+++ App.js
@@ -79,7 +79,7 @@ passport.deserializeUser(User.deserializeUser());//Generates a function that is 
 App.use((req,res,next)=>{
     res.locals.success=req.flash("success")
     res.locals.error=req.flash("error")
-    res.locals.curruser=req.user;
+    res.locals.curruser = req.user || null;
     next();
 })
 // App.get("/",(req,res)=>{
